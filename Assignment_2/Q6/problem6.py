db9bb3fc622c1c8f2c919707e37eebc172332e8a
VIBHOR AGARWAL <vibhor20349@iiitd.ac.in>
2022-10-13 22:53:56 +0530

    Problem 6 Done

---
+++ Assignment_2/Q6/problem6.py
@@ -0,0 +1,28 @@
+import scipy.linalg as sp
+import numpy as np
+import matplotlib.pyplot as plt
+
+def solveQR(k,A,b):
+    Q,R = np.linalg.qr(A)
+    x = sp.solve_triangular(R, np.matmul(Q.T, b))
+    return x
+
+def solveNormal(k,A,b):
+    Ata = np.matmul(A.T, A)
+    Atb = np.matmul(A.T, b)
+    x = np.linalg.solve(Ata, Atb)
+    return x
+
+for i in range(6,16):
+    k=i
+    A = np.array([[1, 1], [10**(-k), 0],[0, 10**(-k)]])
+    b = np.array([-(10**(-k)), 1+10**(-k),1-10**(-k)])
+    print("k = ", i)
+    print("QR: ", solveQR(i,A,b))
+
+for i in range(6,16):
+    k=i
+    A = np.array([[1, 1], [10**(-k), 0],[0, 10**(-k)]])
+    b = np.array([-(10**(-k)), 1+10**(-k),1-10**(-k)])
+    print("k = ", i)
+    print("Normal: ", solveNormal(i,A,b))
