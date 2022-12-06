5ffe791df93c39666126a2232e1b5c881a3df23b
VIBHOR AGARWAL <vibhor20349@iiitd.ac.in>
2022-12-06 23:23:57 +0530

    Minor Changes

--- Assignment_4/problem5.py
+++ Assignment_4/problem5.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from tabulate import tabulate
 hvalues = []
 for i in range(1,16):
     hvalues.append(10**(-i))
@@ -13,13 +14,20 @@ def exactDifferential(x):
     diff = (-0.75)*(x**2)*cosTerm*expTerm
     return diff
 
+def function(x):
+    return math.exp(-math.sin(x**3)/4)
+
 def forwardDifferential(x, h):
-    return((exactDifferential(x+h) - exactDifferential(x))/h)
+    return((function(x+h) - function(x))/h)
 
 for h in hvalues:
     relativeError.append(math.log10(abs((forwardDifferential(1, h) - exactDifferential(1))/exactDifferential(1))))
 
 hvalues = [ math.log10(h) for h in hvalues ]
+table = []
+for i in range(len(hvalues)):
+    table.append([hvalues[i], relativeError[i]])
+print(tabulate(table, headers=['log(h)', 'log(relative error)'], tablefmt= 'fancy_grid'))
 plt.plot(hvalues, relativeError)
 plt.xlabel('h')
 plt.ylabel('Relative Error')
